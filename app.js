04082639b1a95c50b97061772c69f94419092002
rajatlnweb <rajat.lnwebworks@gmail.com>
2019-04-12 17:44:25 +0530

    changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 var server = require('./lib/server');
 var configManager = require('./lib/config-manager');
 var packageInfo = require('./package.json');
-var configFile = process.argv[2] ? process.argv[2] : process.cwd() + '/nodejs.config.js';
+var configFile = process.argv[2] ? process.argv[2] : process.cwd() + '/node_modules/drupal-node.js/nodejs.config.js';
 
 global.version = packageInfo.version;
 
